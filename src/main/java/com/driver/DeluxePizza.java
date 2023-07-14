f7e09e9e66cc50e47e65aa4ca9bdb0c755c684af
Kartargit <skartar61962@gmail.com>
2023-07-14 17:14:19 +0530

    pizzaBilling

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,5 +5,8 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        totalPrice = 400;
+            setPrice(400);
+
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,30 +5,64 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-
+    static int extraCheese=0;
+    static int extraTopping=0;
+    static int takeAway=0;
+    static int totalPrice =0;
+    static String bills = "";
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        totalPrice = 300;
+            setPrice(300);
     }
-
+    public void setPrice(int price){
+        this.price = price;
+    }
     public int getPrice(){
         return this.price;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        extraCheese = 80;
+        totalPrice += 80;
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg)extraTopping = 70;
+        else extraTopping =120;
+        totalPrice += extraTopping;
+
     }
 
     public void addTakeaway(){
         // your code goes here
+        takeAway = 20;
+        totalPrice += takeAway;
+
+    }
+
+      static String base = "Base Price Of The Pizza: ";
+
+      static String cheese = "Extra Cheese Added: ";
+      static String topping = "Extra Toppings Added: ";
+      static String bag = "Paperbag Added: ";
+      static String total = "Total Price: ";
+
+    private void setBill(String bills) {
+        bills = bills+base+getPrice()+"\n";
+       if(extraCheese>0) bills = bills+cheese+extraCheese+"\n";
+        if(extraTopping>0)bills = bills+topping+extraTopping+"\n";
+        if(takeAway>0)bills = bills+bag+takeAway+"\n";
+        this.bill = bills+total+totalPrice+"\n";
     }
 
     public String getBill(){
         // your code goes here
+        setBill(bills);
         return this.bill;
     }
 }
